29ae409fbd748bb21f657467e5b83631d669725f
Paul Gundarapu <leogps@gmail.com>
2025-12-06 05:48:37 -0800

    gzip as package.

--- package-gzip.js
+++ package-gzip.js
@@ -1,11 +1,38 @@
-// compress files into tar.gz archive
-require('targz').compress({
-    src: 'dist/',
-    dest: 'file-uploader.tar.gz'
-}, function(err){
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Packaging done.");
+const targz = require("targz");
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+try {
+    console.log("Installing production dependencies...");
+    execSync("npm ci --only=production", { stdio: "inherit" });
+
+    if (fs.existsSync("file-uploader.tar.gz")) {
+        fs.unlinkSync("file-uploader.tar.gz");
     }
-});
+
+    console.log("Creating package archive...");
+
+    targz.compress(
+        {
+            src: ".",
+            dest: "file-uploader.tar.gz",
+            tar: {
+                entries: [
+                    "dist",
+                    "node_modules",
+                    "package.json",
+                    "package-lock.json"
+                ]
+            }
+        },
+        (err) => {
+            if (err) {
+                console.error("Packaging error:", err);
+            } else {
+                console.log("Packaging complete: file-uploader.tar.gz");
+            }
+        }
+    );
+} catch (err) {
+    console.error("Error preparing package:", err);
+}
